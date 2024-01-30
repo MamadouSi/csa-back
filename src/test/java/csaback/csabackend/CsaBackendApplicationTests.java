b8bfccd5e8a1e732a85de234fefa42cf6e4fd558
Mamadou SIDIBE <mamadousidibe.3116@gmail.com>
2024-01-30 16:56:30 +0100

    First

---
+++ src/test/java/csaback/csabackend/CsaBackendApplicationTests.java
@@ -0,0 +1,10 @@
+package csaback.csabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsaBackendApplicationTests {
+
+
+}
